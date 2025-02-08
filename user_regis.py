7213ac491b01d6459abd1a6194d4b6398847ce71
Ashwin <ash123.8055@gmail.com>
2025-02-08 11:50:11 +0530

    [Ashwin] Add: Added the email validation to the code

--- user_regis.py
+++ user_regis.py
@@ -14,8 +14,16 @@ def last_name_validation(last_name):
     else:
         print("The Last Name is not valid.")
 
+def email_validation(email):
+    pattern=r'^[A-Za-z].*[a-zA-Z0-9].*@[a-zA-Z].*\.[a-zA-Z].*[a-zA-Z]$'
+    if re.findall(pattern,email):
+        print("The email is valid.")
+    else:
+        print("The email is not valid.")
 
 first_name=input("Enter the First Name: ")
 last_name=input("Enter the Last Name: ")
+email=input("Enter the email: ")
 first_name_validation(first_name)
 last_name_validation(last_name)
+email_validation(email)
