bef706e274f40091d6073e8f469d0365322535ed
Ashwin <ash123.8055@gmail.com>
2025-02-10 13:58:56 +0530

    [Ashwin] Add: Added the password rule for minimum 1 numeric number in the password

--- user_regis.py
+++ user_regis.py
@@ -44,7 +44,7 @@ def main():
             print("The Mobile Number is not valid")
 
     def password_rule(password):
-        pattern=r'(?=.*[A-Z])[a-zA-Z0-9]{8,}'
+        pattern=r'(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}'
         if re.match(pattern,password):
             logger.info("The password is valid.")
             print("The Password is valid")
@@ -52,15 +52,15 @@ def main():
             logger.error("The password is not valid")
             print("The password is not valid")
     
-    first_name=input("Enter the First Name: ")
-    last_name=input("Enter the Last Name: ")
-    email=input("Enter the email: ")       
-    mobile_num=input("Enter the mobile number: ")
+    # first_name=input("Enter the First Name: ")
+    # last_name=input("Enter the Last Name: ")
+    # email=input("Enter the email: ")       
+    # mobile_num=input("Enter the mobile number: ")
     password=input("Enter the password: ")
-    first_name_validation(first_name)
-    last_name_validation(last_name)
-    email_validation(email)
-    mobile_num_validation(mobile_num)
+    # first_name_validation(first_name)
+    # last_name_validation(last_name)
+    # email_validation(email)
+    # mobile_num_validation(mobile_num)
     password_rule(password)
 
 if __name__=="__main__":
